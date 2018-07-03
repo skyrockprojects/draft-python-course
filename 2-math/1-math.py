7423529eee31a7f6c32ad4e48df5891d31feab1e
Declan Li-Carney <21116960@tasfiledomain.tas.edu.tw>
2018-07-03 11:44:52 +0800

    renamed math

--- 2-math/1-math.py
+++ 2-math/1-math.py
@@ -1,17 +1,17 @@
 def add(a, b):
-    return "INSERT CODE HERE"
+    #INSERT CODE HERE
     
 def subtract(a, b):
-    return "INSERT CODE HERE"
+    #INSERT CODE HERE
 
 def multiply(a, b):
-    return "INSERT CODE HERE"
+    #INSERT CODE HERE
 
 def divide(a, b):
-    return "INSERT CODE HERE"
+    #INSERT CODE HERE
 
 def module(a, b):
-    return "INSERT CODE HERE"
+    #INSERT CODE HERE
 
 
     #ADD TESTING HERE
