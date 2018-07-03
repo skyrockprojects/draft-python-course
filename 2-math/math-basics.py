aa0797d7d93ab2540141e216d25d6bbf7f0e3e2b
freshkoala <zunigaruizb@gmail.com>
2018-07-03 13:12:07 +0800

    stuff

---
+++ 2-math/math-basics.py
@@ -0,0 +1,32 @@
+import math;
+""" Here, we're importing a library titled math so we can use their various functions. To use a function from math, you must first place math. in front of the function. For example, math.pow(x, y) """
+
+def add(a, b):
+    """ INSERT CODE IN HERE """
+
+def subtract(a, b):
+    """ INSERT CODE IN HERE """
+
+def multiply(a, b):
+    """ INSERT CODE IN HERE """
+
+def divide(a, b):
+    """ INSERT CODE IN HERE """
+
+def module(a,b):
+    """ INSERT CODE IN HERE """
+    """ Module is a commonly used function in programming. It's represented using a percentage sign (%) and gives you the remainder of two numbers. For example, 5 % 2 would be equal to 1 since the remainder of 5 divided by 2 is 1. """
+
+def absolute(a):
+    """ INSERT CODE IN HERE """
+    """ To get the absolute value of a number, you can use the abs(number) function where you insert the number you want to get the absolute value from into the function. For example abs(5 - 6) would return 1 since the absolute value of 5 - 6 is 1. """
+
+def power(a, b):
+    """ INSERT CODE IN HERE """
+    """ To get a raised to the power of b, you're going to have to use the math library, more specifically, the pow function. You must insert two variables into the function. For example math.pow(2,3) would return 2 raised to the power of 3, which is 8 """
+
+def square_root(a):
+    """ INSERT CODE IN HERE """
+    """ Once again, you're going to have to use a function from the math library to get the square root of a number. The sqrt(x) functions returns the square root of x. For example, math.sqrt(16) would return the square root of 16, which is 4 """
+
+""" Isert test runs here, you can print out the results using print() """
