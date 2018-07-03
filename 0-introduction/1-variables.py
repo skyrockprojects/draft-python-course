757a2702106590f4e684fe7d24d672306030a8f7
Declan Li-Carney <21116960@tasfiledomain.tas.edu.tw>
2018-07-03 11:58:05 +0800

    added Variables example and activity

---
+++ 0-introduction/1-variables.py
@@ -0,0 +1,14 @@
+exampleVariable = 24
+
+#change this variable so it copy's exampleVariable's value
+changeThisVariable = "change this value"
+
+print("changeThisVariable = ",changeThisVariable)
+
+#do not change this
+changeThisVariable = "THIS WAS CHANGED"
+
+print("exampleVariable = ",exampleVariable)
+
+#even though changeThisVariable took exampleVariable's value and was changed, exampleVariable did not change
+#changeing one variable cannot change another
---
+++ 0-introduction/ex-1-variables.py
@@ -0,0 +1,20 @@
+#Variables
+
+#Variables, like in math, represent a value, however unlike Algebraic variables Python variables can hold anything
+
+#This represents an Integer
+exampleInteger = 3
+
+#This variable reprents a String
+exampleString = "This is a String"
+
+#This variable represents a list of things
+exampleList = [0,1,2,3,4,5]
+
+#Variables can even hold other variables like this:
+exampleCopy = exampleString
+
+print(exampleInteger)
+print(exampleString)
+print(exampleList)
+print(exampleCopy)
