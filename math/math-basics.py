0e5415caf2471df68f91d0fa66b92c53a2b8d458
freshkoala <zunigaruizb@gmail.com>
2018-07-04 09:29:40 +0800

    commit

--- math/math-basics.py
+++ math/math-basics.py
@@ -1,33 +1,3 @@
 import math;
 #Here, we're importing a library titled math so we can use their various functions. To use a function from math, you must first place math. in front of the function. For example, math.pow(x, y)
 #Here are all the functions in the math library: https://www.programiz.com/python-programming/modules/math
-
-def add(a, b):
-    #Insert code here
-
-def subtract(a, b):
-    #Insert code here
-
-def multiply(a, b):
-    #Insert code here
-
-def divide(a, b):
-    #Insert code here
-
-def module(a,b):
-     #Insert code here
-     #Module is a commonly used function in programming. It's represented using a percentage sign (%) and gives you the remainder of two numbers. For example, 5 % 2 would be equal to 1 since the remainder of 5 divided by 2 is 1. 
-
-def absolute(a):
-     #Insert code here 
-     #To get the absolute value of a number, you can use the abs(number) function where you insert the number you want to get the absolute value from into the function. For example abs(5 - 6) would return 1 since the absolute value of 5 - 6 is 1. 
-
-def power(a, b):
-     #Insert code here 
-     #To get a raised to the power of b, you're going to have to use the math library, more specifically, the pow function. You must insert two variables into the function. For example math.pow(2,3) would return 2 raised to the power of 3, which is 8 
-
-def square_root(a):
-     #Insert code here 
-     #Once again, you're going to have to use a function from the math library to get the square root of a number. The sqrt(x) functions returns the square root of x. For example, math.sqrt(16) would return the square root of 16, which is 4 
-
-#Isert test runs here, you can print out the results using print() 
