8ecd4d4c141e7301686c90233405faf8c937938b
Declan Li-Carney <21116960@tasfiledomain.tas.edu.tw>
2018-07-04 13:21:07 +0800

    added boolean example

---
+++ booleans/ex-booleans.py
@@ -0,0 +1,10 @@
+# booleans (bool) are a very simple data type they can be True or False
+exampleTrue = True
+exampleFalse = False
+
+#the if statement will only run its code if its input is true
+
+if exampleTrue:
+    print("This prints because exampleTrue, the argument (input), is true")
+
+if exa
