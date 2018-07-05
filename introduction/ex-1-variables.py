a77093418edef1b19e0f7c20240f4993147abcb1
Declan Li-Carney <awesomeriptide@gmail.com>
2018-07-05 09:45:21 +0800

    added exampleBoolean

--- introduction/ex-1-variables.py
+++ introduction/ex-1-variables.py
@@ -11,6 +11,9 @@ exampleString = "This is a String"
 #This variable represents a list of things
 exampleList = [0,1,2,3,4,5]
 
+#This variable represents a True or False value (boolean)
+exampleBoolean = True
+
 #Variables can even hold other variables like this:
 exampleCopy = exampleString
 
