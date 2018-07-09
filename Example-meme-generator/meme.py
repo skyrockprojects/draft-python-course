b8dab6798c9ad0364a623b24ab5bc20933606bba
Declan Li-Carney <awesomeriptide@gmail.com>
2018-07-09 10:24:35 +0800

    ads

--- Example-meme-generator/meme.py
+++ Example-meme-generator/meme.py
@@ -5,12 +5,12 @@ url = "https://i.ytimg.com/vi/lEPc9xi0Tt0/hqdefault.jpg"
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
 draw = ImageDraw.Draw(img)
-txt = "Obunga"
-fontsize = 1
+topText = "Obunga"
+topFontSize = 1
 img_fraction = 0.15
-font = ImageFont.truetype("impact.ttf", fontsize)
-while font.getsize(txt)[1] < img_fraction*img.size[1]:
-    fontsize += 1
-    font = ImageFont.truetype("impact.ttf", fontsize)
-draw.text((img.size[0]/2-font.getsize(txt)[0]/2,0),txt,(255,255,255),font=font)
+topFont = ImageFont.truetype("impact.ttf", topFontSize)
+while topFont.getsize(topText)[1] < img_fraction*img.size[1]:
+    topFontSize += 1
+    topFont = ImageFont.truetype("impact.ttf", topFontSize)
+draw.text((img.size[0]/2-topFont.getsize(topText)[0]/2,0),topText,(255,255,255),font=topFont)
 img.save('sample-out.jpg')
