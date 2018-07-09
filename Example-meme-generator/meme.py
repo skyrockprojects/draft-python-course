75b1516ffc4676f0002f0625763cf6e45393c148
Declan Li-Carney <awesomeriptide@gmail.com>
2018-07-09 10:16:26 +0800

    memez

--- Example-meme-generator/meme.py
+++ Example-meme-generator/meme.py
@@ -5,4 +5,12 @@ url = "https://i.ytimg.com/vi/lEPc9xi0Tt0/hqdefault.jpg"
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("sans-serif.ttf", 16)
+txt = "Obunga"
+fontsize = 1
+img_fraction = 0.15
+font = ImageFont.truetype("impact.ttf", fontsize)
+while font.getsize(txt)[1] < img_fraction*img.size[1]:
+    fontsize += 1
+    font = ImageFont.truetype("impact.ttf", fontsize)
+draw.text((img.size[0]/2-font.getsize(txt)[0]/2,0),txt,(255,255,255),font=font)
+img.save('sample-out.jpg')
