c168a61a60b6148705c9e2f652f9d346a497557c
Declan Li-Carney <awesomeriptide@gmail.com>
2018-07-09 10:30:10 +0800

    obunga

--- Example-meme-generator/meme.py
+++ Example-meme-generator/meme.py
@@ -13,4 +13,13 @@ while topFont.getsize(topText)[1] < img_fraction*img.size[1]:
     topFontSize += 1
     topFont = ImageFont.truetype("impact.ttf", topFontSize)
 draw.text((img.size[0]/2-topFont.getsize(topText)[0]/2,0),topText,(255,255,255),font=topFont)
+
+bottomText = "Obunga"
+bottomFontSize = 1
+img_fraction = 0.15
+bottomFont = ImageFont.truetype("impact.ttf", bottomFontSize)
+while bottomFont.getsize(bottomText)[1] < img_fraction*img.size[1]:
+    bottomFontSize += 1
+    bottomFont = ImageFont.truetype("impact.ttf", bottomFontSize)
+draw.text((img.size[0]/2-bottomFont.getsize(bottomText)[0]/2,img.size[1]-bottomFont.getsize(bottomText)[1]),bottomText,(255,255,255),font=bottomFont)
 img.save('sample-out.jpg')
