9da066861da24b3dffc090df4d0a11de5eaa9e2e
freshkoala <zunigaruizb@gmail.com>
2018-07-11 09:32:01 +0800

    commit

--- Project-chat-bot/chatbot-runner.py
+++ Project-chat-bot/chatbot-runner.py
@@ -1,8 +1,6 @@
 # A simple class to run the Chatbot class.
-# @author Laurie White
-#  @version April 2012
 from chatbot import ChatBot
-#Create a Magpie, give it user input, and print its replies.
+#Create a ChatBot, give it user input, and print its replies.
 cb = ChatBot()
 
 print(cb.getGreeting())
